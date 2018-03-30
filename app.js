c000784726c370a8a91a71bcab61fd34b8b09e51
davidbobyang <davidbobyang@gmail.com>
2018-03-30 01:51:54 -0400

    added error message for no search results, added correct pluralization

--- app.js
+++ app.js
@@ -18,12 +18,18 @@ $(document).ready(function() {
 
 
 				var cap = json.results.length;
-				if (cap > 5) {
-					cap = 5;
+
+				if (cap == 0) {
+					$('#output').append("<p>oops, we couldn't find anything :(</p>");
 				}
-				for (var i = 0; i < cap; i++) {
-					$('#output').append("<a href='javascript:;' class='result' id='" + i + "'></a>");
-					document.getElementById(i).innerHTML = json.results[i].name;
+				else {
+					if (cap > 5) {
+						cap = 5;
+					}
+					for (var i = 0; i < cap; i++) {
+						$('#output').append("<a href='javascript:;' class='result' id='" + i + "'></a>");
+						document.getElementById(i).innerHTML = json.results[i].name;
+					}
 				}
 
 				$('#search-results').show();
@@ -34,34 +40,50 @@ $(document).ready(function() {
 					id = json.results[event.target.id].id;
 
 					$.ajax({
-					type: 'GET',
-					url: 'https://api.themoviedb.org/3/tv/' + id + '?language=en-US&api_key=5114cf314283a1d83f54f9684a701572',
-					async: false,
-					jsonpCallback: 'testing',
-					contentType: 'application/json',
-					dataType: 'jsonp',
+						type: 'GET',
+						url: 'https://api.themoviedb.org/3/tv/' + id + '?language=en-US&api_key=5114cf314283a1d83f54f9684a701572',
+						async: false,
+						jsonpCallback: 'testing',
+						contentType: 'application/json',
+						dataType: 'jsonp',
 
-					success: function(json) {
-						$('#search-results').hide();
+						success: function(json) {
+							$('#search-results').hide();
 
-						var run_time = json.episode_run_time;
-						if (run_time.constructor === Array) {
-							run_time = run_time[0];
+							var run_time = json.episode_run_time;
+							if (run_time.constructor === Array) {
+								run_time = run_time[0];
+							}
+
+							var total = json.number_of_episodes * run_time;
+							var seasons = json.number_of_seasons;
+							var time_per_season = total / seasons;
+							var hours = total / 60;
+							var days = parseInt(hours / 24);
+							var remainder_hours = parseInt(hours) % 24;
+
+							var sentence = json.name + ' takes ';
+							if (days != 0) {
+								if (days == 1) {
+									sentence += days + ' day and ';
+								}
+								else {
+									sentence += days + ' days and ';
+								}
+							}
+							if (remainder_hours == 1) {
+								sentence += remainder_hours + ' hour to watch';
+							}
+							else {
+								sentence += remainder_hours + ' hours to watch';
+							}
+
+							document.getElementById('final-output').innerHTML = sentence;
+						},
+						error: function(e) {
+							console.log(e.message);
 						}
-
-						var total = json.number_of_episodes * run_time;
-						var seasons = json.number_of_seasons;
-						var time_per_season = total / seasons;
-						var hours = total / 60;
-						var days = parseInt(hours / 24);
-						var remainder_hours = parseInt(hours) % 24;
-
-						document.getElementById('final-output').innerHTML = json.name + ' takes ' + days + ' days and ' + remainder_hours + ' hours to watch';
-					},
-					error: function(e) {
-						console.log(e.message);
-					}
-				});
+					});
 				});
 			},
 			error: function(e) {
