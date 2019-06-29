16d24d4b1d45cbc55f24f1715729d54c2bd23cde
davidbobyang <davidbobyang@gmail.com>
2019-06-29 18:34:55 -0400

    Updates

---
+++ precache-manifest.51b843b3a6d901b5e74db60074799384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86ee6eabb964dc1c298dbb593b79f31",
+    "url": "/bingetime/index.html"
+  },
+  {
+    "revision": "a6dfad8212411d217d1f",
+    "url": "/bingetime/static/css/main.62cad877.chunk.css"
+  },
+  {
+    "revision": "8af27f66309633ebe488",
+    "url": "/bingetime/static/js/2.6933908c.chunk.js"
+  },
+  {
+    "revision": "a6dfad8212411d217d1f",
+    "url": "/bingetime/static/js/main.071eee6b.chunk.js"
+  },
+  {
+    "revision": "d9c9fd2c2a748f60900c",
+    "url": "/bingetime/static/js/runtime~main.ab54a8a1.js"
+  },
+  {
+    "revision": "1e555263b948a6575b878bbe0e32eccb",
+    "url": "/bingetime/static/media/popcorn.1e555263.png"
+  }
+]);
